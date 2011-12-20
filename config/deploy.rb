c6fb34e14bf8eca53ada2bce97fb5a142771d72f
Foy Savas <foy@sav.as>
2011-12-19 22:22:01 -0500

    deployment scripts

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+set :application, "techbubbler"
+set :repository,  "git://github.com/foysavas/techbubbler.git"
+
+set :scm, :git
+set :branch, "master"
+set :deploy_to, "/data/techbubbler"
+
+role :app, "techbubbler.com"
+set :user, "root"
+
+namespace :deploy do
+  task :start do ; end
+  task :end do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "mkdir -p #{deploy_to}/current/config/prod; ln -s #{deploy_to}/shared/config/redis.conf #{deploy_to}/current/config/prod"
+    run "rm -rf #{deploy_to}/current/tmp; ln -s #{deploy_to}/shared/tmp #{deploy_to}/current/tmp"
+    run "god restart #{application}"
+  end
+end
